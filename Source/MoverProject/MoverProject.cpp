f645fd992b92a6211107f52662d9d91fe1593265
Valk <v4lk@duck.com>
2025-09-26 12:28:23 +0200

    Project Initialisation

---
+++ Source/MoverProject/MoverProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoverProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MoverProject, "MoverProject" );
